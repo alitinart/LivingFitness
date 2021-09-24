b27d477e31f733302e1989449bf541af3eb42bea
alitinart <alitinart@gmail.com>
2021-09-24 23:06:49 +0200

    Adding Files

--- src/app/training/biceps/biceps.page.ts
+++ src/app/training/biceps/biceps.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-biceps',
@@ -6,9 +7,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./biceps.page.scss'],
 })
 export class BicepsPage implements OnInit {
-  time = 10;
+  maxTime = 10;
+  timer;
+  started = false;
+  text = 'Lay flat and grab the weight';
+  paused = false;
+  currentTime = 0;
 
   constructor() {}
+
+  onStart() {
+    this.started = true;
+    this.paused = false;
+
+    this.timer = setTimeout((x) => {
+      if (this.maxTime <= 0) {
+      }
+      this.maxTime -= 1;
+
+      if (this.maxTime > 0) {
+        this.started = true;
+        this.onStart();
+      } else {
+        this.started = false;
+        this.maxTime = 30;
+        this.text = 'Now do 10 reps, each rep lasting 3 seconds inbetween';
+      }
+    }, 1000);
+  }
+
+  onPause() {
+    this.paused = true;
+    this.currentTime = this.maxTime;
+    this.maxTime = 0;
+    this.started = false;
+  }
+
+  onUnPause() {
+    this.maxTime = this.currentTime;
+    this.started = true;
+    this.paused = false;
+    this.onStart();
+  }
 
   ngOnInit() {}
 }
